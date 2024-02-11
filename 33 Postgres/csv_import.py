708685b43ea7d6f16c7d2b9d95a2555fd7730d96
FelixF <felixfyx@gmail.com>
2024-02-11 22:38:32 +0800

    Cleared postgres writing and reading

---
+++ 33 Postgres/csv_import.py
@@ -0,0 +1,26 @@
+###
+# This is created because I was using docker containers for my postgres and 
+# pgadmin
+###
+import psycopg2
+import os
+
+conn = psycopg2.connect(
+    host="localhost",
+    dbname="world",
+    user="felixfoo",
+)
+
+print("Currently connected")
+cur = conn.cursor()
+
+# Debug
+print(os.getcwd())
+
+with open('./33 Postgres/flags.csv', 'r') as f:
+    next(f) # Skip header row
+    cur.copy_expert('COPY flags FROM stdin WITH CSV', f)
+
+conn.commit()
+
+print("Successfully added into list")
